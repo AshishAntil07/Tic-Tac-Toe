566ccf73e587bb6d50b3e6468281057b244f2648
Ashish Antil <ashishantil.antil07@gmail.com>
2022-12-13 20:56:47 +0530

    Update Tic Tac.js

--- docs/Tic Tac.js
+++ docs/Tic Tac.js
@@ -10,31 +10,24 @@ const U2Box = document.querySelector(".U2");
 const PABtn = document.querySelector("#playAgainBtn");
 const U1score = document.querySelector("#U1Score");
 const U2score = document.querySelector("#U2Score");
-const Boxes = document.querySelector('.mainTicTacContainer').childNodes;
-let turn=1, P1Score=0, P2Score=0, toRemove=[], moves=0;
+const Boxes = [...document.querySelector('.mainTicTacContainer').childNodes];
+let turn=1, P1Score=0, P2Score=0, moves=0;
 document.querySelector('.GameContainer').style.height = document.querySelector(".nameInputForm").style.height = `${screen.height}px`;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   if(U1name.value.length > 12 || U2name.value.length > 12){
     alert("Player's name cannot be larger than 12 characters.");
-    return;
-  }else{
-    window.location.href="#'";
-  }
+    return
+  }else window.location.href="#'";
   P1name.innerHTML = U1name.value;
   P2name.innerHTML = U2name.value;
   startNew();
 })
 
-Boxes.forEach((elem, index) => index%2===0?toRemove.push(elem):0)
-toRemove.forEach(elem => elem.remove());
+Boxes.forEach((elem, index) => index%2===0?elem.remove():0)
 
-const removeListeners = () => {
-  for(let j = 0; j < Boxes.length; j++){
-    Boxes[j].removeEventListener('click', boxFunc)
-  }
-}
+const removeListeners = () => Boxes.forEach(box=>box.removeEventListener('click', boxFunc));
 
 let checkWin = () => {
   if(Boxes[0].circle === true && Boxes[1].circle === true && Boxes[2].circle === true || Boxes[3].circle === true && Boxes[4].circle === true && Boxes[5].circle === true || Boxes[6].circle === true && Boxes[7].circle  === true && Boxes[8].circle === true || Boxes[0].circle === true && Boxes[3].circle === true && Boxes[6].circle === true || Boxes[0].circle === true && Boxes[4].circle === true && Boxes[8].circle === true || Boxes[2].circle === true && Boxes[4].circle === true && Boxes[6].circle === true || Boxes[1].circle === true && Boxes[4].circle === true && Boxes[7].circle === true || Boxes[2].circle === true && Boxes[5].circle === true && Boxes[8].circle === true){
@@ -55,37 +48,18 @@ let checkWin = () => {
 };
 
 const boxFunc = e => {
-  e.target.innerHTML = `${
-    turn === 0 ? "<img src='Pictures/Tic Tac Cross.png'>"
-    : "<img src='Pictures/Tic Tac Circle.png'>"
-  }`
+  e.target.innerHTML = `<img src='Pictures/Tic Tac ${turn===0?'Cross':'Circle'}.png'>`
   turn === 0 ? e.target.cross = true : e.target.circle = true;
   turn === 1 ? turn = 0 : turn++; moves++
   checkWin();clickCheck()
 }
 
 function clickCheck(){
-  if(turn === 0){
-    U2Box.style.borderStyle = "solid";
-    U2Box.style.borderWidth = "1px";
-    U2Box.style.borderColor = "white";
-
-    U1Box.style.borderStyle = "solid";
-    U1Box.style.borderColor = "gray";
-    U1Box.style.borderWidth = "1px";
-  }else if(turn === 1){
-    U1Box.style.borderStyle = "solid";
-    U1Box.style.borderWidth = "1px";
-    U1Box.style.borderColor = "white";
-
-    U2Box.style.borderStyle = "solid";
-    U2Box.style.borderColor = "gray";
-    U2Box.style.borderWidth = "1px";
-  };
+  U1Box.style.border=`1px solid ${turn?'white':'gray'}`;
+  U2Box.style.border=`1px solid ${turn?'gray':'white'}`;
 };
 
 const startNew = newGame => {
-  debugger;
   moves = 0;
   winner.innerHTML = ``;
   turn = parseInt(Math.random()*2);
@@ -93,16 +67,15 @@ const startNew = newGame => {
   clickCheck()
 
   if(newGame){
-    P1Score = 0;
-    P2Score = 0;
+    P1Score = P2Score = 0;
     U1score.innerHTML = U2score.innerHTML = `Score: 0`;
   }
 
-  for(let i=0; i<Boxes.length; i++){
-    Boxes[i].innerHTML = '';
-    Boxes[i].circle = false; Boxes[i].cross = false;
-    Boxes[i].addEventListener("click", boxFunc, {once:true});
-  }
+  Boxes.forEach(box=>{
+    box.innerHTML='';
+    box.circle=box.cross=false;
+    box.addEventListener('click', boxFunc, {once:true});
+  })
 }
 
 PABtn.addEventListener("click", ()=>{startNew(false)});
